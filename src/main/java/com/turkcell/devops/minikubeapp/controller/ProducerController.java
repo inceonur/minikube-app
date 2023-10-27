70e044554d0036f35bad233994466ef0f22cc304
ONUR INCE <ince.onur@turkcell.com.tr>
2023-10-27 15:41:41 +0300

    kafka part added

---
+++ src/main/java/com/turkcell/devops/minikubeapp/controller/ProducerController.java
@@ -0,0 +1,23 @@
+package com.turkcell.devops.minikubeapp.controller;
+
+
+import com.turkcell.devops.minikubeapp.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("minikube-java/api")
+public class ProducerController {
+    @Autowired
+    private ProducerService service;
+    @RequestMapping(value="/greet", method= RequestMethod.GET)
+    public String greet() {
+        return "Welcome Java App!";
+    }
+    @GetMapping("/message/{message}")
+    public String message(@PathVariable String message) {
+        service.sendMessage(message);
+        return "Message sent!";
+    }
+
+}
